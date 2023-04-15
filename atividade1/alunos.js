6b6a0c78401c5b9625a6b17489b2ef7a6c761293
https://github.com/EuricoOliveira/html.git <https://github.com/EuricoOliveira/html.git>
2023-04-15 19:33:05 -0300

    Atividade

--- atividade1/alunos.js
+++ atividade1/alunos.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 const alunos = [
     { nome: 'João Silva', media: 7.5 },
     { nome: 'Maria Santos', media: 8.0 },
@@ -13,10 +15,54 @@ const alunos = [
   function filtrarPorMedia(media) {
     return alunos.filter(aluno => aluno.media >= media);
   }
+
+  function adicionarAluno(aluno) {
+    alunos.push(aluno);
+    fs.writeFile('db.json', JSON.stringify({ alunos }), function(err) {
+      if (err) {
+        console.log("Erro ao escrever arquivo", err);
+      } else {
+        console.log("Arquivo escrito com sucesso!");
+      }
+    });
+  }
+
+  function atualizarAluno(matricula, alunoAtualizado) {
+    const alunoIndex = alunos.findIndex(aluno => aluno.matricula === matricula);
+    if (alunoIndex >= 0) {
+      alunos[alunoIndex] = alunoAtualizado;
+      fs.writeFile('db.json', JSON.stringify({ alunos }), function(err) {
+        if (err) {
+          console.log("Erro ao escrever arquivo", err);
+        } else {
+          console.log("Arquivo escrito com sucesso!");
+        }
+      });
+    }
+  }
+
+  function removerAluno(index) {
+    const i = parseInt(index);
+    if (isNaN(i) || i < 0 || i >= alunos.length) {
+      return false; // retorna falso se o aluno não for encontrado
+    }
+    const alunoRemovido = alunos.splice(i, 1)[0];
+    fs.writeFile('db.json', JSON.stringify({ alunos }), function(err) {
+      if (err) {
+        console.log("Erro ao escrever arquivo", err);
+      } else {
+        console.log("Arquivo escrito com sucesso!");
+      }
+    });
+    return alunoRemovido; // retorna o aluno removido se encontrado
+  }
   
   module.exports = {
     alunos,
     filtrarPorNome,
-    filtrarPorMedia
+    filtrarPorMedia,
+    adicionarAluno,
+    atualizarAluno,
+    removerAluno
   };
   
--- atividade1/index.js
+++ atividade1/index.js
@@ -6,37 +6,59 @@ app.use(express.json());
 
 
 app.get("/alunos", (req, res) => {
-    const { nome, media } = req.query;
-  
-    let alunosFiltrados = listaAlunos.alunos;
-  
-    if (nome) {
-      alunosFiltrados = listaAlunos.filtrarPorNome(nome);
-    }
-  
-    if (media) {
-      alunosFiltrados = listaAlunos.filtrarPorMedia(media);
-    }
-  
-    res.json(alunosFiltrados);
-  });
+  const { nome, media } = req.query;
+
+  let alunosFiltrados = listaAlunos.alunos;
+
+  if (nome) {
+    alunosFiltrados = listaAlunos.filtrarPorNome(nome);
+  }
+
+  if (media) {
+    alunosFiltrados = listaAlunos.filtrarPorMedia(media);
+  }
+
+  res.json(alunosFiltrados);
+});
 
 
 app.post("/alunos/novo", (req, res) => {
-    const { nome, matricula, media } = req.body;
-    const alunos = []
-  
-    if(!nome || !matricula || !media) {
-      res.status(400).json({message: "Erro, insira os dados corretos!"})
-    } else {
-      alunos.push({ nome, matricula, media});
-      res.json(`Aluno: ${nome}, matricula: ${matricula}, media: ${media}, foi adicionado com sucesso.`);
-    };
-  });
+  const { nome, matricula, media } = req.body;
 
-app.post("/alunos/deletar/:index", (req, res) => {
-  
-})
+  if (!nome || !matricula || !media) {
+    res.status(400).json({ message: "Erro, insira os dados corretos!" });
+  } else {
+    const novoAluno = { nome, matricula, media };
+    listaAlunos.adicionarAluno(novoAluno);
+    res.status(201).json({ message: `Aluno ${nome} com a matrícula ${matricula} e média ${media} foi adicionado com sucesso.` });
+  }
+});
+
+app.delete("/alunos/:index", (req, res) => {
+  const { index } = req.params;
+  const alunoRemovido = listaAlunos.removerAluno(index);
+
+  if(!alunoRemovido) {
+    res.status(404).json({ message: 'Aluno não encontrado.'});
+  } else {
+    res.json(`Aluno(a) ${alunoRemovido.nome} removido com sucesso.`)
+  }
+});
+
+app.put("/alunos/:index", (req, res) => {
+  const { index } = req.params
+  const { nome, media } = req.body;
+  const aluno = listaAlunos.alunos[index];
+
+  if(!aluno) {
+    res.status(404).json({ message: 'Aluno não encontrado.'});
+  } else {
+    listaAlunos.atualizarAluno(index, nome, media);
+    res.json(`Aluno ${nome} atualizado com sucesso.`)
+  }
+});
+
+
 
 app.listen(3000, () => {
     console.log("Servidor rodando em http://localhost:3000/");
